45fdf581e7b17c8998fffe07e9e908d40a575e83
Astle-Ligo <astlelijo04@gmail.com>
2025-02-21 04:13:04 +0530

    admin helpers changed

--- helpers/admin-helpers.js
+++ helpers/admin-helpers.js
@@ -1,106 +1,111 @@
-var db = require('../config/connection')
-
-var collection = require('../config/collection')
-
-const bcrypt = require('bcrypt')
-
-const { ObjectId } = require('mongodb')
-const async = require('hbs/lib/async')
-const { reject } = require('bcrypt/promises')
-const async = require('hbs/lib/async')
-const { reject } = require('bcrypt/promises')
+var db = require('../config/connection');
+var collection = require('../config/collection');
+const bcrypt = require('bcrypt');
+const { ObjectId } = require('mongodb');
 
 module.exports = {
     doSignup: (adminData) => {
         return new Promise(async (resolve, reject) => {
-            adminData.Password = await bcrypt.hash(adminData.Password, 10)
-            db.get().collection(collection.ADMIN_COLLECTION).insertOne(adminData).then((data) => {
-                adminData._id = data.insertedId
-                resolve(adminData)
-            })
-        })
-
+            try {
+                adminData.Password = await bcrypt.hash(adminData.Password, 10);
+                let data = await db.get().collection(collection.ADMIN_COLLECTION).insertOne(adminData);
+                adminData._id = data.insertedId;
+                resolve(adminData);
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     doLogin: (adminData) => {
         return new Promise(async (resolve, reject) => {
-            let loginStatus = false
-            let response = {}
-            let admin = await db.get().collection(collection.ADMIN_COLLECTION).findOne({ Email: adminData.Email })
+            try {
+                let response = {};
+                let admin = await db.get().collection(collection.ADMIN_COLLECTION).findOne({ Email: adminData.Email });
 
-            if (admin) {
-                bcrypt.compare(adminData.Password, admin.Password).then((status) => {
+                if (admin) {
+                    let status = await bcrypt.compare(adminData.Password, admin.Password);
                     if (status) {
                         console.log("Login Success");
-                        response.admin = admin
-                        response.status = true
-                        resolve(response)
+                        response.admin = admin;
+                        response.status = true;
+                        resolve(response);
                     } else {
                         console.log("Login Failed");
-                        resolve({ status: false })
+                        resolve({ status: false });
                     }
-                })
-            } else {
-                console.log("No User");
-                resolve({ status: false })
+                } else {
+                    console.log("No User Found");
+                    resolve({ status: false });
+                }
+            } catch (error) {
+                reject(error);
             }
-        })
+        });
     },
 
     findAdminCount: () => {
         return new Promise(async (resolve, reject) => {
-
-            let count = await db.get().collection(collection.ADMIN_COLLECTION).count()
-
-            if (count > 0) {
-                resolve({ status: true })
-            } else {
-                resolve({ status: false })
+            try {
+                let count = await db.get().collection(collection.ADMIN_COLLECTION).countDocuments();
+                resolve({ status: count > 0 });
+            } catch (error) {
+                reject(error);
             }
-        })
+        });
     },
 
     addClue: (clue, callback) => {
-        db.get().collection(collection.CLUE_COLLECTION).insertOne(clue).then((data) => {
-            callback(data.insertedId.toString())
-        })
+        db.get().collection(collection.CLUE_COLLECTION).insertOne(clue)
+            .then((data) => callback(data.insertedId.toString()))
+            .catch((error) => console.error("Error adding clue:", error));
     },
 
     getAllClues: () => {
         return new Promise(async (resolve, reject) => {
-            let clues = await db.get().collection(collection.CLUE_COLLECTION).find().toArray()
-            resolve(clues)
-        })
+            try {
+                let clues = await db.get().collection(collection.CLUE_COLLECTION).find().toArray();
+                resolve(clues);
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     getClueDetails: (clueId) => {
-        return new Promise((resolve, reject) => {
-            db.get().collection(collection.CLUE_COLLECTION).findOne({ _id: new ObjectId(clueId) }).then((clue) => {
-                resolve(clue)
-            })
-        })
+        return new Promise(async (resolve, reject) => {
+            try {
+                let clue = await db.get().collection(collection.CLUE_COLLECTION).findOne({ _id: new ObjectId(clueId) });
+                resolve(clue);
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     updateClue: (clueId, clueDetails) => {
-        return new Promise((resolve, reject) => {
-            db.get().collection(collection.CLUE_COLLECTION).updateOne({ _id: new ObjectId(clueId) }, {
-                $set: {
-                    clueNumber: clueDetails.clueNumber,
-                    clue: clueDetails.clue,
-                    answer: clueDetails.answer
-                }
-            }).then((response) => {
-                resolve()
-            })
-        })
+        return new Promise(async (resolve, reject) => {
+            try {
+                await db.get().collection(collection.CLUE_COLLECTION).updateOne(
+                    { _id: new ObjectId(clueId) },
+                    { $set: { clueNumber: clueDetails.clueNumber, clue: clueDetails.clue, answer: clueDetails.answer } }
+                );
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     deleteClue: (clueId) => {
-        return new Promise((resolve, reject) => {
-            db.get().collection(collection.CLUE_COLLECTION).deleteOne({ _id: new ObjectId(clueId) }).then((response) => {
-                resolve(response)
-            })
-        })
+        return new Promise(async (resolve, reject) => {
+            try {
+                let response = await db.get().collection(collection.CLUE_COLLECTION).deleteOne({ _id: new ObjectId(clueId) });
+                resolve(response);
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     getGameSettings: async () => {
@@ -117,72 +122,45 @@ module.exports = {
 
     getAllUsers: () => {
         return new Promise(async (resolve, reject) => {
-            let users = await db.get().collection(collection.USER_COLLECTION).find().toArray()
-            resolve(users)
-        })
-    },
-
-    getUserResponses: () => {
-        return new Promise(async (resolve, reject) => {
-            let responses = await db.get().collection(collection.USER_RESPONSE_COLLECTION).find().toArray()
-            resolve(responses)
-        })
-    },
-
-    getLeaderboard: () => {
-        return new Promise(async (resolve, reject) => {
             try {
-                let leaderboard = await db.get().collection(collection.USER_COLLECTION).aggregate([
-                    {
-                        $project: {
-                            _id: 1,
-                            teamName: "$Name",
-                            collegeName: "$collegeName",
-                            lastCompletedClue: {
-                                $toInt: {
-                                    $ifNull: [
-                                        { $toString: "$lastCompletedClue" }, // Convert to string first
-                                        "0" // Default to "0" if null/undefined
-                                    ]
-                                }
-                            },
-                            timeTaken: "$formattedTime",
-                            totalTimeTaken: {
-                                $toInt: {
-                                    $ifNull: [
-                                        { $toString: "$totalTimeTaken" }, // Convert to string first
-                                        "0" // Default to "0" if null/undefined
-                                    ]
-                                }
-                            }
-                        }
-                    },
-                    { $sort: { lastCompletedClue: -1, totalTimeTaken: 1 } } // Rank by level & time
-                ]).toArray();
-
-                resolve(leaderboard);
+                let users = await db.get().collection(collection.USER_COLLECTION).find().toArray();
+                resolve(users);
             } catch (error) {
                 reject(error);
             }
         });
     },
 
-
-
-
     getUserResponses: () => {
         return new Promise(async (resolve, reject) => {
-            let responses = await db.get().collection(collection.USER_RESPONSE_COLLECTION).find().toArray()
-            resolve(responses)
-        })
+            try {
+                let responses = await db.get().collection(collection.USER_RESPONSE_COLLECTION).find().toArray();
+                resolve(responses);
+            } catch (error) {
+                reject(error);
+            }
+        });
     },
 
     getLeaderboard: async () => {
         try {
-            const leaderboard = await db.get().collection(collection.USER_COLLECTION)
-                .find({}, { projection: { name: 1, college: 1, level: 1, totalTime: 1 } }) // Fetch required fields
-                .sort({ level: -1, totalTime: 1 }) // Sort by level (desc) and time taken (asc)
-                .toArray();
+            let leaderboard = await db.get().collection(collection.USER_COLLECTION).aggregate([
+                {
+                    $project: {
+                        _id: 1,
+                        teamName: "$Name",
+                        collegeName: "$collegeName",
+                        lastCompletedClue: {
+                            $toInt: { $ifNull: [{ $toString: "$lastCompletedClue" }, "0"] }
+                        },
+                        timeTaken: "$formattedTime",
+                        totalTimeTaken: {
+                            $toInt: { $ifNull: [{ $toString: "$totalTimeTaken" }, "0"] }
+                        }
+                    }
+                },
+                { $sort: { lastCompletedClue: -1, totalTimeTaken: 1 } }
+            ]).toArray();
 
             return leaderboard;
         } catch (error) {
@@ -190,5 +168,4 @@ module.exports = {
             throw error;
         }
     }
-
-}
+};
